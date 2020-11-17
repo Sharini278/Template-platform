5597b4beb84ac7c4668b754977ec6a0f810dc813
Sharini278 <chandrika.saggi@gmail.com>
2020-11-17 16:39:16 +0000

    added platform

--- sketch.js
+++ sketch.js
@@ -14,10 +14,10 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+    platform = new Ground(150, 305, 300, 170);
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -58,7 +58,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
-    bird.display();
     platform.display();
+    bird.display();
+
 }
